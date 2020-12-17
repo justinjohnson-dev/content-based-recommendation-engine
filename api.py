97be5ef3c052219beb5edba5732d58d117592b37
Luke <luke@reetayn.com>
2020-12-16 20:08:52 -0600

    Renamed for import and added call

--- api.py
+++ api.py
@@ -1,9 +1,10 @@
 from flask import Flask
-
+from reccomendation_engine import get_recommendation
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    get_recommendation()
     return 'Web App with Python Flask!'
 
 app.run(host='0.0.0.0', port=81)
--- reccomendation_engine.py
+++ reccomendation_engine.py
@@ -9,19 +9,35 @@ from sklearn.feature_extraction.text import CountVectorizer
 # df = df.head(20000)
 
 ## IF using free endpoint to grab 250 movies for testing
-df = pd.read_csv('https://query.data.world/s/uikepcpffyo2nhig52xxeevdialfl7')
+def get_recommendation(): # movie_name
+    df = pd.read_csv('https://query.data.world/s/uikepcpffyo2nhig52xxeevdialfl7')
 
-# Setting df columns to lowercase to work for both use cases
-df.columns = [x.lower() for x in df.columns]
+    # Setting df columns to lowercase to work for both use cases
+    df.columns = [x.lower() for x in df.columns]
+    
+    # grabbing only the columns we want to compare with
+    df = df[['title','genre','director','actors']]
+    # setting columns to strs to avoid any errors in
+    # the get_important_features function
+    df['title'] = df['title'].astype(str)
+    df['genre'] = df['genre'].astype(str)
+    df['director'] = df['director'].astype(str)
+    df['actors'] = df['actors'].astype(str)
 
-# grabbing only the columns we want to compare with
-df = df[['title','genre','director','actors']]
-# setting columns to strs to avoid any errors in
-# the get_important_features function
-df['title'] = df['title'].astype(str)
-df['genre'] = df['genre'].astype(str)
-df['director'] = df['director'].astype(str)
-df['actors'] = df['actors'].astype(str)
+    # create the column to hold combined strings
+    df['important_features'] = get_important_features(df)
+
+    # convert text to a matrix of token counts for important_features
+    cm = CountVectorizer().fit_transform(df['important_features'])
+    # get the cosine similarity matrix from count matrix
+    cs = cosine_similarity(cm)
+    print(cs)
+
+    # Build a 1-dimensional array with movie titles
+    titles = df['title']
+    indices = pd.Series(df.index, index=df['title'])
+    recommendations('The Dark Knight', indices, cs, titles).head(10)
+
 
 # combine values into a column that we will use for comparison
 def get_important_features(data):
@@ -30,21 +46,8 @@ def get_important_features(data):
         important_features.append(data['title'][i]+' '+data['genre'][i]+' '+data['director'][i]+' '+data['actors'][i])
     return important_features
 
-# create the column to hold combined strings
-df['important_features'] = get_important_features(df)
-
-# convert text to a matrix of token counts for important_features
-cm = CountVectorizer().fit_transform(df['important_features'])
-# get the cosine similarity matrix from count matrix
-cs = cosine_similarity(cm)
-print(cs)
-
-# Build a 1-dimensional array with movie titles
-titles = df['title']
-indices = pd.Series(df.index, index=df['title'])
-
 # Function that get movie recommendations based on the cosine similarity score of movie genres
-def recommendations(title):
+def recommendations(title, indices, cs, titles):
     try:
         idx = indices[title]
         sim_scores = list(enumerate(cs[idx]))
@@ -63,4 +66,3 @@ def recommendations(title):
         print('Movie was not found in dataset')
         print('')
 
-recommendations('The Dark Knight').head(10)
